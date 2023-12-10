67760094ecff47dde97285f361c8d8c25e4cf1e3
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2023-12-10 16:09:18 +0900

    Add MiniCssExtractPlugin to babel.config.js

--- Sample-App-Src/Calculator/babel.config.js
+++ Sample-App-Src/Calculator/babel.config.js
@@ -2,8 +2,9 @@
 * Copyright (c) Microsoft Corporation. All rights reserved.
 * Licensed under the MIT License.
 *--------------------------------------------------------------------------------------------*/
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ]  
 }
