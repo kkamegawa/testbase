e97d5f56a09bd428d65c6b40fc4cfce7e9e4f8e3
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2023-12-10 15:11:52 +0900

    Add node.js compatibility flag to build script

--- Sample-App-Src/Calculator/build.ps1
+++ Sample-App-Src/Calculator/build.ps1
@@ -1,3 +1,5 @@
+# for node.js compatibility
+$env:NODE_OPTIONS = "--openssl-legacy-provider"
 dotnet publish "./CalculatorCLI" -r win-x64 -c Release -o "./CalculatorCLI/drop"
 & npm install
 & npm run pack
